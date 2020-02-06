4c36c6265625b898d6f500d0a2c08b916e0b7cd0
jitojar <jitojar@100.67.200.103>
2020-02-06 10:53:41 +0000

    Made the halls static so they are accessible by every controller

--- src/accomodationsystem/ManagerViewController.java
+++ src/accomodationsystem/ManagerViewController.java
@@ -7,13 +7,18 @@ package accomodationsystem;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
 /**
@@ -23,13 +28,22 @@ import javafx.stage.Stage;
  */
 public class ManagerViewController implements Initializable {
     
-
+    @FXML
+    private TableView<ArrayList<Hall>> table;
+    
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        
+        //populate the tablex
+        AccomodationSystem.halls.get(0);
+        
+        //table.setItems(AccomodationSystem.data.getHalls());
     }    
     
     /**
